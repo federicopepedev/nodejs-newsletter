f826104164542c7a3725e1e688aab16c37a6717a
federicopepedev <federicopepedev@gmail.com>
2024-02-01 19:00:49 +0100

    built in bp

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const https = require('https');
 const app = express()
 const port = 3000
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/signup.html');
